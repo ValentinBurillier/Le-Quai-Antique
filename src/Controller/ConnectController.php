b0411288e7da837ba92c57be01c190e4d5a04e3c
Valentin Burillier <valentin.burillier@gmail.com>
2023-03-01 15:00:50 +0100

    create connect page and responsive

--- src/Controller/ConnectController.php
+++ src/Controller/ConnectController.php
@@ -2,26 +2,17 @@
 
 namespace App\Controller;
 
-use App\Form\ConnectType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConnectController extends AbstractController
 {
     #[Route('/connect', name: 'app_connect')]
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        $form = $this->createForm(ConnectType::class);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            dd($form->getData());
-        }
         return $this->render('connect/index.html.twig', [
-            'form' => $form->createView(),
+            'controller_name' => 'ConnectController',
         ]);
     }
 }
