46b46d416da5c4afd057320df32dd95585941265
Valentin Burillier <valentin.burillier@gmail.com>
2023-03-03 14:47:42 +0100

    create users list html & css and responsive

---
+++ src/Controller/UsersListController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsersListController extends AbstractController
+{
+    #[Route('/users/list', name: 'app_users_list')]
+    public function index(): Response
+    {
+        return $this->render('users_list/index.html.twig');
+    }
+}
