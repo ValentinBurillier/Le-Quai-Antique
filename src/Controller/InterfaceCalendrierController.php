630207dd4f17a781d0ef96181603dc9e2fc8b179
Valentin Burillier <valentin.burillier@gmail.com>
2023-03-13 15:47:29 +0100

    start & end calendrier admin page html, css and responsive

---
+++ src/Controller/InterfaceCalendrierController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InterfaceCalendrierController extends AbstractController
+{
+    #[Route('/interface/calendrier', name: 'app_interface_calendrier')]
+    public function index(): Response
+    {
+        $displayArrowLeft = true;
+        return $this->render('interface_calendrier/index.html.twig', [
+            'controller_name' => 'InterfaceCalendrierController',
+            'displayArrowLeft' => $displayArrowLeft,
+        ]);
+    }
+}
