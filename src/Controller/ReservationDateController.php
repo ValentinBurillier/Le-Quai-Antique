39209d4503d4b95867179e94f0604592b50e2a52
Valentin Burillier <valentin.burillier@gmail.com>
2023-03-13 10:50:03 +0100

    start reservation page html & css 320 * 568px

---
+++ src/Controller/ReservationDateController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReservationDateController extends AbstractController
+{
+    #[Route('/reservation/date', name: 'app_reservation_date')]
+    public function index(): Response
+    {
+        return $this->render('reservation_date/index.html.twig');
+    }
+}
