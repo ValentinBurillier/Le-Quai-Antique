16d3d59055abbec293f7465ed9b6cc6aea7ff356
Valentin Burillier <valentin.burillier@gmail.com>
2023-03-14 14:57:41 +0100

    start details reservation page html & css

---
+++ src/Controller/ReservationDetailsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReservationDetailsController extends AbstractController
+{
+    #[Route('/reservation/details', name: 'app_reservation_details')]
+    public function index(): Response
+    {
+        return $this->render('reservation_details/index.html.twig');
+    }
+}
