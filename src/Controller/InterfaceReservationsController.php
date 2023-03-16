a15483584926be81a9dc2d9b32e09f05c8f41d0b
Valentin Burillier <valentin.burillier@gmail.com>
2023-03-16 22:50:37 +0100

    interface reservations page responsive

--- src/Controller/InterfaceReservationsController.php
+++ src/Controller/InterfaceReservationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ReservationsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class InterfaceReservationsController extends AbstractController
 {
     #[Route('/interface/reservations', name: 'app_interface_reservations')]
-    public function index(): Response
+    public function index(ReservationsRepository $reservationsRepository): Response
     {
+        /* RECUPERATION DES DONNEES ET TRIER */
+        $datas = $reservationsRepository->findBy(array(), array('dateofreservation' => 'asc'));
         return $this->render('interface_reservations/index.html.twig', [
             'controller_name' => 'InterfaceReservationsController',
+            'datas' => $datas,
         ]);
     }
 }
