992f3ec990d1ffa0b4f1670dd310b5483df38acb
Valentin Burillier <valentin.burillier@gmail.com>
2023-03-16 10:15:27 +0100

    create felicitation page html, css and responsive

--- src/Controller/FelicitationController.php
+++ src/Controller/FelicitationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRestaurantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class FelicitationController extends AbstractController
 {
     #[Route('/felicitation', name: 'app_felicitation')]
-    public function index(): Response
+    public function index(ContactRestaurantRepository $contactRestaurantRepository): Response
     {
-        return $this->render('felicitation/index.html.twig');
+        $linkButton = '/';
+        $disabled = 'enable';
+        $getPhoneNumber = $contactRestaurantRepository->find(1)->getPhonenumber();
+        $phoneNumber = "0$getPhoneNumber";
+        return $this->render('felicitation/index.html.twig', [
+            'phoneNumber' => $phoneNumber,
+            'disabled' => $disabled,
+            'linkButton' => $linkButton,
+
+        ]);
     }
 }
