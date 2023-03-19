160f44c70b9cceea55cdd79966421c9c2ce0c246
Valentin Burillier <valentin.burillier@gmail.com>
2023-03-19 18:05:05 +0100

    start images home page html, css and responsive

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -11,13 +11,32 @@ class AccueilController extends AbstractController
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
+        $y = 0;
+        $linkButton = '/reservation';
+        $disabled = 'enable';
+        $textbtn = 'Réservez votre table dès maintenant';
+
+        /* IMPORT FIRST HOME PAGE IMAGES */
         $resources = scandir('./assets/images/recettes/');
         $repertory = './assets/images/recettes/';
         unset($resources[0]);
         unset($resources[1]);
+
+        /* IMPORT SECOND IMAGES PAGE */
+        $imgs = scandir('./assets/images/plats/');
+        $platPath = './assets/images/plats/';
+        unset($imgs[0]);
+        unset($imgs[1]);
+
         return $this->render('accueil/index.html.twig', [
             'repertory' => $repertory,
             'resources' => $resources,
+            'platImgs' => $imgs,
+            'platPath' => $platPath,
+            'y' => $y,
+            'linkButton' => $linkButton,
+            'disabled' => $disabled,
+            'textbtn' => $textbtn,
         ]);
     }
 }
