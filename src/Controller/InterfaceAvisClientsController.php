92f2d641081b884d9204941e4017c00c7c546753
Valentin Burillier <valentin.burillier@gmail.com>
2023-03-20 11:14:23 +0100

    review part 1 admin interface pages

--- src/Controller/InterfaceAvisClientsController.php
+++ src/Controller/InterfaceAvisClientsController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\ReviewsTypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InterfaceAvisClientsController extends AbstractController
 {
-    #[Route('/interface/avis/clients', name: 'app_interface_avis_clients')]
-    public function index(): Response
+    #[Route('/interface/avis-clients', name: 'app_interface_avis_clients')]
+    public function index(ReviewsTypeRepository $reviewsTypeRepository): Response
     {
+        $datas = $reviewsTypeRepository->findAll();
         return $this->render('interface_avis_clients/index.html.twig', [
-            'controller_name' => 'InterfaceAvisClientsController',
+            'datas' => $datas,
         ]);
     }
 }
--- src/Controller/UsersListController.php
+++ src/Controller/UsersListController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\InfosUsersRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,8 +12,36 @@ class UsersListController extends AbstractController
 {
 
     #[Route('/interface/users-list', name: 'app_users_list')]
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, InfosUsersRepository $infosUsersRepository): Response
     {
-        return $this->render('users_list/index.html.twig');
+        /* INIT VARIABLES */
+        $thirdArray = [];
+        $lastName = '';
+        $firstName = '';
+        $phoneNumber = '';
+
+        /* GET EMAILS AND STOCK IN USERS VARIABLE */
+        $datas = $userRepository->findAll();
+        $users = [];
+        foreach($datas as $data) {
+            $users[] = $data->getEmail();
+        }
+
+        $secondArray = [];
+        foreach($users as $user) {
+            $secondArray[] = $infosUsersRepository->findBy([
+                'email' => $user,
+            ]);
+            
+            foreach($secondArray as $datasUsers) {
+                if ($datasUsers !== []) {
+                    $thirdArray[] = $datasUsers;
+                }
+            }
+        }
+
+        return $this->render('users_list/index.html.twig', [
+            'thirdArray' => $thirdArray,
+        ]);
     }
 }
